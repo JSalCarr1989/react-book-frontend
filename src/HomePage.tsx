863b71760479855f8235875e33eff3dbca7fa779
Salvador Carrillo <Salvador.Carrillo@magna.com>
2020-05-26 13:59:59 -0500

    codigo del libro

---
+++ src/HomePage.tsx
@@ -0,0 +1,56 @@
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core';
+import { PrimaryButton } from './Styles';
+import { QuestionList } from './QuestionList';
+import { getUnansweredQuestions, QuestionData } from './QuestionData';
+import { Page } from './Page';
+import { PageTitle } from './PageTitle';
+import { useEffect, useState } from 'react';
+
+// const renderQuestion = (question: QuestionData) => <div>{question.title}</div>;
+
+export const HomePage = () => {
+  const [questions, setQuestions] = useState<QuestionData[] | null>(null);
+  const [questionLoading, setQuestionLoading] = useState(true);
+
+  useEffect(() => {
+    // const questions = await getUnansweredQuestions();
+    const doGetUnansweredQuestions = async () => {
+      const unansweredQuestions = await getUnansweredQuestions();
+      setQuestions(unansweredQuestions); //<---- seteamos las preguntas
+      setQuestionLoading(false); // <--- seteamos el state de isFetching
+    };
+    doGetUnansweredQuestions();
+  }, []);
+
+  // console.log('rendered');
+
+  return (
+    <Page>
+      <div
+        css={css`
+          display: flex;
+          align-items: center;
+          justify-content: space-between;
+        `}
+      >
+        <PageTitle>Unanswered Questions</PageTitle>
+
+        <PrimaryButton>Ask a question</PrimaryButton>
+      </div>
+
+      {questionLoading ? (
+        <div
+          css={css`
+            font-size: 16px;
+            font-size: italic;
+          `}
+        >
+          Loading...
+        </div>
+      ) : (
+        <QuestionList data={questions || []} />
+      )}
+    </Page>
+  );
+};
---
+++ src/Icons.tsx
@@ -0,0 +1,15 @@
+// import React from 'react';
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core';
+import user from './user.svg';
+
+export const UserIcon = () => (
+  <img
+    src={user}
+    alt="User"
+    css={css`
+      width: 12px;
+      opacity: 0.6;
+    `}
+  />
+);
